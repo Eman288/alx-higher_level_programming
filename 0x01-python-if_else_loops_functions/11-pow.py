7bacd7d01dec8a5875460f607ed37e8154ca845d
Eman288 <emantamam107@gmail.com>
2023-08-09 10:03:03 -0700

    0x01-task11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    c = 1
-    if b >= 0:
-        for i in range(b):
-            c *= a
-    else:
-        b *= -1
-        for i in range(b):
-            c /= a
-    return c
+    return a ** b
