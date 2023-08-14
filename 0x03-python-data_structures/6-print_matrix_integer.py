fafde00cb9445b093374ba6e8cd145ddcaa21edb
Eman288 <emantamam107@gmail.com>
2023-08-13 20:00:01 -0700

    0x03-task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            if j != i[-1]:
-                print("{:d}".format(i[j]), end=" ")
-            else:
-                print("{:d}".format(i[j]), end="")
-        print()
+                print("{:d}".format(i[j]), end=" " if j != i[-1] else "")
+        print("")
