82f59246bf6007736403e0a862b5063318cd1242
Eman288 <emantamam107@gmail.com>
2023-09-06 18:21:34 -0700

    0x08-task2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,43 @@
+class Rectangle:
+    """
+    a class to create a rectangle with width and hight
+    """
+
+    def __init__(self, width=0, height=0):
+        """a function to initialize the variables
+        width => the width
+        height => the height
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return (self.__height + self.__width) * 2
