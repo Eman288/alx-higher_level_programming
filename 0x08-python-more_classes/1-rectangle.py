d2c759b49a96edfca322bab4d24614273821be94
Eman288 <emantamam107@gmail.com>
2023-09-06 18:11:22 -0700

    0x08-task1#study motegit add .

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,9 +16,11 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    @property
     def width(self):
         return self.__width
 
+    @width.setter
     def width(self, value):
         if isinstance(value, int) is not True:
             raise TypeError("width must be an integer")
@@ -26,9 +28,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
         return self.__height
 
+    @height.setter
     def height(self, value):
         if isinstance(value, int) is not True:
             raise TypeError("height must be an integer")
