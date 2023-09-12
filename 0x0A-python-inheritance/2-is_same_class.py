f5af20f7bc5134ac1935f492cd72e640df7b9471
Eman288 <emantamam107@gmail.com>
2023-09-11 18:47:41 -0700

    0x0A-task2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a module that has the is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    if type(obj) == a_class:
+            return True
+    return False
