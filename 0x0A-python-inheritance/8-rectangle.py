aad5f4551f5ceeabf7c5becc52a5742f18b8b45b
Eman288 <emantamam107@gmail.com>
2023-09-11 19:36:46 -0700

    0x0A-task8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defintion of the class"""
+
+
+class BaseGeometry:
+    """a class with a method that raise an exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """a class rectangle that inherits from basegeometry class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
