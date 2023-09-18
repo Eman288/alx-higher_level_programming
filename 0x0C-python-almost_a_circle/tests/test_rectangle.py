a34073be4c19a2e53c198657c535c23dd3c18df9
Eman288 <emantamam107@gmail.com>
2023-09-18 10:41:18 -0700

    0x0C-task1

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -11,19 +11,22 @@ from models.base import Base
 from models.rectangle import Rectangle
 """the class we will test on"""
 
-class TestRectangle(unittest.TestCase):
+class TestRectangle_init(unittest.TestCase):
     """the main testing class"""
 
     def test_only_two(self):
+        """a function to test two arguemnt condition"""
         a1 = Rectangle(10, 2)
         a2 = Rectangle(2, 10)
         self.assertEqual(a1.id, a2.id - 1)
 
     def test_only_three(self):
+        """a function to test three arguemnt condition"""
         b = Rectangle(4, 5, 6)
         self.assertEqual(b.x, b.width + 2)
 
     def test_only_four(self):
+        """a function to test four arguemnt condition"""
         c = Rectangle(8, 5, 1, 4)
         self.assertEqual(c.width, c.y + 4)
 
