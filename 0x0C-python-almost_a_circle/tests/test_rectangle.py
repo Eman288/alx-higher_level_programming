dd0ba1a1642bad7b2feb8a9da2d405d86ab4d514
Eman288 <emantamam107@gmail.com>
2023-09-18 10:37:03 -0700

    0x0C-task2

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""test module for rectangle"""
+
+
+import unittest
+"""the module we will test with"""
+
+from models.base import Base
+"""the base class"""
+
+from models.rectangle import Rectangle
+"""the class we will test on"""
+
+class TestRectangle(unittest.TestCase):
+    """the main testing class"""
+
+    def test_only_two(self):
+        a1 = Rectangle(10, 2)
+        a2 = Rectangle(2, 10)
+        self.assertEqual(a1.id, a2.id - 1)
+
+    def test_only_three(self):
+        b = Rectangle(4, 5, 6)
+        self.assertEqual(b.x, b.width + 2)
+
+    def test_only_four(self):
+        c = Rectangle(8, 5, 1, 4)
+        self.assertEqual(c.width, c.y + 4)
+
+    if __name__ == "__main__":
+        unittest.main()
