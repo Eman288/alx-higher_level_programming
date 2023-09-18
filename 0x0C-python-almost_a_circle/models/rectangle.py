771004e6b33f5f8cde8b1e61c43d2c87bd1522a9
Eman288 <emantamam107@gmail.com>
2023-09-18 11:20:05 -0700

    0x0C-task3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,40 +10,81 @@ class Rectangle(Base):
     """the Rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """initiate the values"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.width = width
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.height = height
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.x = x
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.y = y
         Base.__init__(self, id)
     
     @property
     def width(self):
+        """get the width"""
         return self.__width
 
     @width.setter
     def width(self, width):
+        """set the width"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
     
     @property
     def height(self):
+        """get the height"""
         return self.__height
     
     @height.setter
     def height(self, height):
+        """set the height"""
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
     def x(self):
+        """get the x"""
         return self.__x
     
     @x.setter
     def x(self, x):
+        """set the x"""
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
     def y(self):
+        """get the y"""
         return self.__y
     
     @y.setter
     def y(self, y):
-        self.__y = y 
+        """set the y"""
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("x must be >= 0")
+        self.__y = y
--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -29,6 +29,63 @@ class TestRectangle_init(unittest.TestCase):
         """a function to test four arguemnt condition"""
         c = Rectangle(8, 5, 1, 4)
         self.assertEqual(c.width, c.y + 4)
+class TestRectangle_width(unittest.TestCase):
+    """the class testing of width"""
+    def test_str(self):
+        """a function to test the str value"""
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+                Rectangle("fd", 2)
+    
+    def test_less_than_0(self):
+        """a function to test if a number less than 0"""
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+                Rectangle(-3, 2)
+                
+    def test_equal_to_0(self):
+        """a function to test if a number equal to 0"""
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+                Rectangle(0, 2)
+                
+class TestRectangle_height(unittest.TestCase):
+    """the class testing of height"""
+    def test_str(self):
+        """a function to test the str value"""
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+                Rectangle(32, "dsa")
+    
+    def test_less_than_0(self):
+        """a function to test if a number less than 0"""
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+                Rectangle(3, -2)
+                
+    def test_equal_to_0(self):
+        """a function to test if a number equal to 0"""
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+                Rectangle(2, 0)
+        
+class TestRectangle_x(unittest.TestCase):
+    """the class testing of x"""
+    def test_str(self):
+        """a function to test the str value"""
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+                Rectangle(4, 2, "fxd")
+    
+    def test_less_than_0(self):
+        """a function to test if a number less than 0"""
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+                Rectangle(3, 2, -3)
+                
+class TestRectangle_y(unittest.TestCase):
+    """the class testing of y"""
+    def test_str(self):
+        """a function to test the str value"""
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+                Rectangle(4, 2, 3, "DSa")
+    
+    def test_less_than_0(self):
+        """a function to test if a number less than 0"""
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+                Rectangle(3, 2, 3, -3)
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
         unittest.main()
