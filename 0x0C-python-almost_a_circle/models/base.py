315ecc9b2f3dbcff684ebc593a62373c32881ae5
Eman288 <emantamam107@gmail.com>
2023-09-20 06:01:55 -0700

    0x0C-task17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,8 +41,9 @@ class Base:
                     li.append(i.to_dictionary())
                 f.write(Base.to_json_string(li))
 
+    @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
         if json_string is None or len(json_string) == 0:
-            return "[]"
+            return []
         return json.loads(json_string)
