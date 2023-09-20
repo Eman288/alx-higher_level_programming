c072801d226de3b0d60d7be4c6fe59e4d20c5b1f
Eman288 <emantamam107@gmail.com>
2023-09-19 23:38:03 -0700

    0x0C-task13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -141,7 +141,8 @@ class Rectangle(Base):
                 self.__x = kwargs.get(i)
             elif i == "y":
                 self.__y = kwargs.get(i)
+
     def to_dictionary(self):
         """returns the dictionary representation of a Rectangle"""
         return {'id': self.id, 'width': self.width, 'height': self.height,
-            'x': self.x, 'y': self.y}
+                'x': self.x, 'y': self.y}
