21afb7098e81294bc81e93f7fe5114bcc813f269
Eman288 <emantamam107@gmail.com>
2023-11-15 12:46:42 -0800

    0x12-task7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,8 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
 } else {
   let n = process.argv[2];
-  while (n != 0) {
+  while (n !== 0) {
     console.log('C is fun');
-	  n -= 1;
+    n -= 1;
   }
 }
