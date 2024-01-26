5bb1d4b9e9eb2dc9a12a834f9bfac98f0c9a9947
Eman288 <emantamam107@gmail.com>
2024-01-25 16:25:06 -0800

    0x01-task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 #a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response 
-curl -s -b "$1" | grep -i Content-Length
+curl -s -I "$1" | grep -i Content-Length | cut -d " " -f 2
