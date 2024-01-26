067ff178bdf3b5d3c72836f69071c32be8f9943f
Eman288 <emantamam107@gmail.com>
2024-01-25 18:54:10 -0800

    0x01-task6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    if list_of_integers is None:
+        return None
+    else:
+        p = 0
+        n = 0
+        j = 0
+        for i in range(0, len(list_of_integers)):
+            if i != 0:
+                if list_of_integers[i] >= list_of_integers[i - 1]:
+                    if list_of_integers[i] >= list_of_integers[i + 1]:
+                        return list_of_integers[i]
+            elif i != len(list_of_integers - 1):
+                if list_of_integers[i] >= list_of_integers[i + 1]:
+                    return list_of_integers[i]
+            else:
+                return list_of_integers[i]
